8ab7836b0785a0367671b92ab5732b174b116849
Matt Kaelber <Matt.Kaelber@sita.aero>
2019-06-25 09:57:18 -0400

    fixing styles

--- assignment1/assignment1-starter-code/scripts/app.js
+++ assignment1/assignment1-starter-code/scripts/app.js
@@ -9,17 +9,17 @@
 		$scope.input = "";
 		$scope.customStyle = {};
 		$scope.checkResult = function () {
-			$scope.customStyle.style = { "color": "green" }; //Inherently keep green
+			$scope.customStyle.style = { "color": "green", "border-style":"solid", "border-width":"1px" }; //Inherently keep green
 			var words = $scope.input.split(',');
 			var empty = 0;
-			for (i = 0; i < words.length; i++) {
+			for (var i = 0; i < words.length; i++) {
 				if (words[i].trim() === "") {
 					empty = empty + 1;
 				}
 			}
 			var amount = words.length - empty;
 			if ($scope.input === "" || amount == 0) {
-				$scope.customStyle.style = { "color": "red" };
+				$scope.customStyle.style = { "color": "red", "border-style": "solid", "border-width": "1px" };
 				$scope.result = "Please enter data first";
 			}
 			else if ($scope.input.split(',').length <= 3) {
