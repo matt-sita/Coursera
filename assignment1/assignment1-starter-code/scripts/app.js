97d7d0842d91fdce8230a6489747c95d311025e5
Matt Kaelber <Matt.Kaelber@sita.aero>
2019-06-25 09:53:36 -0400

    adding color and extra fluff

--- assignment1/assignment1-starter-code/scripts/app.js
+++ assignment1/assignment1-starter-code/scripts/app.js
@@ -7,9 +7,19 @@
 	function BarFoo($scope) {
 		$scope.result = "";
 		$scope.input = "";
-
+		$scope.customStyle = {};
 		$scope.checkResult = function () {
-			if ($scope.input === "" || $scope.input.split(',').length == 0) {
+			$scope.customStyle.style = { "color": "green" }; //Inherently keep green
+			var words = $scope.input.split(',');
+			var empty = 0;
+			for (i = 0; i < words.length; i++) {
+				if (words[i].trim() === "") {
+					empty = empty + 1;
+				}
+			}
+			var amount = words.length - empty;
+			if ($scope.input === "" || amount == 0) {
+				$scope.customStyle.style = { "color": "red" };
 				$scope.result = "Please enter data first";
 			}
 			else if ($scope.input.split(',').length <= 3) {
